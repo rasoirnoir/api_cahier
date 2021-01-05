05acee17631615330c38897804525e1dc2b73a4c
William NORIS <w.noris@protonmail.com>
2021-01-05 17:38:44 +0100

    Ajout tournee, pdi

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+import os, datetime
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -25,8 +25,8 @@ class PDI(db.Model):
     depot = db.Column(db.Boolean)
     tournee = db.Column(db.Integer, db.ForeignKey('tournee.id'), nullable=True)
     ordre = db.Column(db.Integer)
-    date_crea = db.Column(db.Date)
-    date_maj = db.Column(db.Date)
+    date_crea = db.Column(db.DateTime)
+    date_maj = db.Column(db.DateTime)
 
     def __init__(self, noms, adresse, depot, tournee, ordre, date_crea, date_maj):
         self.noms = noms
@@ -42,8 +42,8 @@ class Tournee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     numero = db.Column(db.Integer, unique=True)
     # pdis = db.Relationship('PDI', backref='pdi', lazy=True)
-    date_crea = db.Column(db.Date)
-    date_maj = db.Column(db.Date)
+    date_crea = db.Column(db.DateTime)
+    date_maj = db.Column(db.DateTime)
 
     def __init__(self, numero, date_crea, date_maj):
         self.numero = numero
@@ -67,11 +67,48 @@ pdis_schema = PDISchema(many=True)
 tournee_schema = TourneeSchema()
 tournees_schema = TourneeSchema(many=True)
 
+# création d'une tournée
+@app.route("/tournee", methods = ["POST"])
+def add_tournee():
+    numero = request.json["numero"]
+    date_crea = datetime.datetime.now()
+    date_maj = datetime.datetime.now()
+
+    new_tournee = Tournee(numero, date_crea, date_maj)
+    db.session.add(new_tournee)
+    db.session.commit()
+
+    return tournee_schema.jsonify(new_tournee)
+
+# création d'un pdi
+@app.route("/pdi", methods = ["POST"])
+def add_pdi():
+    noms = request.json["noms"]
+    adresse = request.json["adresse"]
+    depot = request.json["depot"]
+    tournee = request.json["tournee"]
+    ordre = request.json["ordre"]
+    date_crea = datetime.datetime.now()
+    date_maj = datetime.datetime.now()
+
+    new_pdi = PDI(noms, adresse, depot, tournee, ordre, date_crea, date_maj)
+    db.session.add(new_pdi)
+    db.session.commit()
+
+    return pdi_schema.jsonify(new_pdi)
 
 
-# @app.route("/", methods=["GET"])
-# def index():
-#     return jsonify({"message": "Mauvaise route"})
+# get all tournées
+@app.route("/tournee", methods = ["GET"])
+def get_tournees():
+    all_tournees = Tournee.query.all()
+    result = tournee_schema.dump(all_tournees)
+    return jsonify(result)
+
+
+
+
+
 
 def main():
     print("{} is running.".format(__file__.split("/")[len(__file__.split("/")) - 1]))
