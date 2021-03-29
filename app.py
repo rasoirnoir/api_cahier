abafedb186cf22f3b2c2f2bc65843838bb57bfe4
William NORIS <w.noris@protonmail.com>
2021-03-29 21:45:58 +0200

    Added Access-Control-Allow-Origin in response header

--- app.py
+++ app.py
@@ -63,6 +63,26 @@ def token_required(f):
         return f(*args, **kwargs)
     return decorator
 
+@app.after_request
+def add_cors_headers(response):
+    # r = request.referrer[:-1]
+    # if r in white:
+    #     response.headers.add('Access-Control-Allow-Origin', r)
+    #     response.headers.add('Access-Control-Allow-Credentials', 'true')
+    #     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    #     response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+    #     response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
+    #     response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+    #     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    response.headers.add('Access-Control-Allow-Origin', "*")
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+    response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
+    response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+    # response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+    return response
+
 
 
 # création d'une tournée
